8c2f0d0c44dd57f49d8811494645ac8c22ed2949
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2022-09-29 09:22:40 -0500

    Play end music sooner

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ import { CardType } from './domain/CardType'
 // These should match the animation values in index.css, where applicable
 export const FLIP_DURATION = 256
 export const OUTCOME_SOUND_DELAY = 384
-export const OUTCOME_AFTER_SOUND_PLAYED = OUTCOME_SOUND_DELAY + 400
+export const OUTCOME_AFTER_SOUND_PLAYED = OUTCOME_SOUND_DELAY + 150
 export const OUTCOME_FLIP_BACK_DELAY = 1280
 export const ENV_REVEAL_MUSIC_DELAY = 512
