d7417804b0c4587cc77221b33d27a705d7b6f56d
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2022-10-01 14:26:28 -0500

    Add note

--- src/components/CreditsScreen.tsx
+++ src/components/CreditsScreen.tsx
@@ -8,7 +8,13 @@ const CreditsScreen: React.FC<Props> = ({ visible }) => {
   if (!visible) return null
   return (
     <div>
-      TODO: Credits
+      TODO:
+        - Thanks for playing
+        - Link to GitHub repo
+        - "Play Again" button
+        - "Reset Scores" button
+        - Animated background and link for https://en.wikipedia.org/wiki/Super_Mario_Clouds
+        - "Falling" music loop X times, fade out via web audio?
     </div>
   )
 }
